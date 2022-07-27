6761d05306de074bf23d85bc9d78f2542b9e50ee
addirossi <addirossi@gmail.com>
2022-07-27 22:38:08 +0600

    Add project

---
+++ logistic/serializers.py
@@ -0,0 +1,55 @@
+from rest_framework import serializers
+
+from logistic.models import StockProduct, Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для продукта
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для позиции продукта на складе
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'price', 'quantity']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    # настройте сериализатор для склада
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        # здесь вам надо заполнить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+        for item in positions:
+            StockProduct.objects.create(stock=stock, **item)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        # здесь вам надо обновить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+        for item in positions:
+            StockProduct.objects.update_or_create(stock=stock,
+                                                  product=item['product'],
+                                                  defaults={'price': item['price'], 'quantity': item['price']})
+
+        return stock
---
+++ logistic/views.py
@@ -0,0 +1,19 @@
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+from django_filters import rest_framework as filters
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
