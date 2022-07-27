dfa6801b2b6d518d995a9b0babfc485d662355d3
addirossi <addirossi@gmail.com>
2022-07-27 23:16:18 +0600

    Prepare project for Heroku

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +136,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 1
 }
+
+django_heroku.settings(locals())
